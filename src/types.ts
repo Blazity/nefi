dcd880588d6f07e0f22a15e184ecb1823637f23a
Igor Klepacki <neg4n@icloud.com>
2025-01-10 11:55:45 +0100

    feat: add diff patching for file modifications

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,10 @@
 import { CommandContext } from "@stricli/core";
 
+export type SourceFile = Readonly<{  
+  path: string;
+  content: string;
+}>
+
 export type ScriptContext = {
   path: string;
   packages?: string[];

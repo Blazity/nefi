23ee28a0933980a21ddbae16585d6cec045542a2
Igor Klepacki <neg4n@icloud.com>
2025-01-21 08:55:18 +0100

    chore: add date check at the start of nefi

--- src/start.ts
+++ src/start.ts
@@ -17,6 +17,7 @@ import pc from "picocolors"
 import { AgentCommand } from "./commands/agent";
 import { TestXmlCommand } from "./commands/test-xml/impl";
 import dedent from "dedent";
+import { isBefore, parseISO } from "date-fns";
 
 const cli = new Cli({
   binaryName: packageJson.name,
@@ -38,6 +39,21 @@ cli.register(AgentCommand);
 cli.register(TestXmlCommand);
 
 console.log(logo, "\n");
+
+const RELEASE_DATE = parseISO('2025-01-23')
+const shouldSkipDateCheck = process.argv.includes('--skip-date-check')
+
+if (shouldSkipDateCheck) {
+  process.argv = process.argv.filter(arg => arg !== '--skip-date-check')
+}
+
+if (isBefore(new Date(), RELEASE_DATE) && !shouldSkipDateCheck) {
+  console.log(" Coming soon... 23th of January, 2025\n")
+  console.log(` ${pc.bgBlack(pc.whiteBright(pc.bold(" https://x.com/")))}${pc.bgBlack(pc.whiteBright(pc.bold("nefi_ai ")))}\n`)
+  console.log(` ${pc.bgBlack(pc.whiteBright(pc.bold(" https://nefi.ai ")))}\n`)
+  process.exit(1)
+}
+
 console.log(dedent`
   ${pc.bold(pc.bgBlazityOrange(pc.black(" Next Enterprise Feature Integrations ")))}
 
@@ -46,5 +62,6 @@ console.log(dedent`
 `)
 
 console.log("")
+
 
 cli.runExit(process.argv.slice(2));

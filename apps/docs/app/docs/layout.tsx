ea9a3f0639e3bae44d16695e7cc5e7409ed2ecde
Jakub Ziemba <ziemba.jak@gmail.com>
2025-01-23 11:55:38 +0100

    fix: switch nefi logo to variant without shadows

--- apps/docs/app/docs/layout.tsx
+++ apps/docs/app/docs/layout.tsx
@@ -3,8 +3,8 @@ import { RootProvider } from 'fumadocs-ui/provider';
 import type { ReactNode } from 'react';
 import { baseOptions } from '@/app/layout.config';
 
-import NefiLogo from "@/public/logo-no-bg-inverted.png";
-import Image from "next/image";
+import NefiLogo from '@/public/logo-no-bg-inverted.png';
+import Image from 'next/image';
 import { source } from '@/lib/source';
 import { XIcon } from '../icons/x';
 import { GithubIcon } from '../icons/github';
@@ -12,43 +12,42 @@ import { GithubIcon } from '../icons/github';
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <RootProvider>
-      <DocsLayout 
-      
-        tree={source.pageTree} 
+      <DocsLayout
+        tree={source.pageTree}
         {...baseOptions}
         links={[
           {
-            text: 
+            text: (
               <div className="flex flex-row items-center gap-1 justify-end">
-                <Image src={NefiLogo} className='mt-1' alt="Nefi Logo" width={45} height={10} />
-                <span><span className="sr-only">nefi </span>repository</span>
+                <Image src={NefiLogo} className="mb-0.5" alt="Nefi Logo" width={45} height={10} />
+                <span>
+                  <span className="sr-only">nefi </span>repository
+                </span>
               </div>
-            ,
-            icon: <GithubIcon className="size-3"/>,
-            url: "https://github.com/blazity/nefi",
+            ),
+            icon: <GithubIcon className="size-3" />,
+            url: 'https://github.com/blazity/nefi',
             external: true,
           },
           {
-            text: "x.com/nefi_ai",
-            url: "https://x.com/nefi_ai",
+            text: 'x.com/nefi_ai',
+            url: 'https://x.com/nefi_ai',
             external: true,
-            icon: <XIcon className="size-3"/>
+            icon: <XIcon className="size-3" />,
           },
           {
-            text: "next-enterprise",
-            url: "https://github.com/blazity/next-enterprise",
+            text: 'next-enterprise',
+            url: 'https://github.com/blazity/next-enterprise',
             external: true,
-            icon: <GithubIcon className="size-3"/>
-          }
+            icon: <GithubIcon className="size-3" />,
+          },
         ]}
         nav={{
-          ...baseOptions.nav, 
+          ...baseOptions.nav,
           title: (
-            <div className="flex flex-col items-start">
+            <div className="flex flex-col items-start gap-2">
               <Image src={NefiLogo} alt="Nefi Logo" width={100} height={100} />
-              <span className="text-xs font-bold">
-                Next.js Configuration AI Agent
-              </span>
+              <span className="text-xs font-bold">Next.js Configuration AI Agent</span>
             </div>
           ),
         }}

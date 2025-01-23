e2d7dc3ad20a7882bc3ba614a0afc0f0877b62ae
Jakub Ziemba <ziemba.jak@gmail.com>
2025-01-23 11:05:48 +0100

    fix: add redirect from root path to /docs

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -5,6 +5,15 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/docs/usage',
+        permanent: true, // Set to true for a 301 redirect
+      },
+    ];
+  },
 };
 
 export default withMDX(config);
